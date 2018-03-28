be242a9c7638be13f4af4dc9898d24cdd011d504
Ambrosio Torres Galvis (Biólogo, Universidad Industrial de Santander) <atorresgalvis@gmail.com>
2018-03-28 15:45:45 -0300

    Update ConSha.R

--- ConSha.R
+++ ConSha.R
@@ -1,9 +1,9 @@
 ## Title: ConSha
 ## Version: 0.1-1
-## Date: 2016-08-24
+## Date: 2018-03-28
 ## Author: Ambrosio Torres
 ## Maintainer: Ambrosio Torres <atorresgalvis@gmail.com>
-## Depends: R version (>= 3.1.0), geomorph, abind
+## Depends: R version (>= 3.4.1), geomorph, abind
 ## Description: obtain the CONsensus (average) SHApes of multiple specimens, for each species in a same tps (.tps) file.
 ## License: GPL (3)
 
@@ -13,7 +13,8 @@ ConSha <- function(x) {   ## x must be the name of the tps file without the ".tp
 		tpsfile_name <- paste(x, ".tps", sep="")
 		data1 <- readland.tps(tpsfile_name, specID = "ID")
 		NameList <- dimnames(data1)[[3]]							
-		UniqueNames <- unique(gsub('[[:digit:]]+', '', NameList))	
+		UniqueNames <- unique(gsub('[[:digit:]]+', '', NameList))
+		UniqueNames <- unique(gsub('[[:space:]]', '', UniqueNames))
 		finalData <- NULL											
 		for(i in 1:length(UniqueNames)) {							
 				name <- UniqueNames[i]
